ef738e23b5e7058964035b1cd45ef6c802da5e79
Pip Taylor <git@evilgeek.co.uk>
2018-09-16 18:32:05 +0100

    Start with a program that fills screen with black

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,31 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@8192
+D=A
+@limit
+M=D
+
+(RESET_OFFSET)
+@offset
+M=0
+
+(START)
+
+@SCREEN
+D=A
+@offset
+A=D+M
+M=0
+M=!M
+
+@offset
+M=M+1
+D=M
+@limit
+D=D-M
+@RESET_OFFSET
+D;JGT
+
+@START
+0;JMP
