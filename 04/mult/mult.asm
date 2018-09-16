d3a61bde510cd3439342382460b722bed415b3e6
Pip Taylor <git@evilgeek.co.uk>
2018-09-16 17:56:59 +0100

    Implemented multiplication in assembly

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,35 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@i
+M=1
+@R1
+D=M
+@R2
+M=0
+
+// Loop R0 times
+(LOOP)
+@i
+D=M
+@R0
+D=D-M
+@END
+D;JGT
+
+// Body of loop
+@R1
+D=M
+@R2
+M=D+M
+
+// Increment i
+@i
+M=M+1
+@LOOP
+0;JMP
+
+// Terminate
+(END)
+@END
+0;JMP
